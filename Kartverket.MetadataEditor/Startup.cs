c93462387640e7691e537be5bba36f4de91f9ab9
Henning Jensen <henning@arkitektum.no>
2013-11-13 09:31:15 +0100

    Bootstrapping with new ASP .NET MVC 5 project

---
+++ Kartverket.MetadataEditor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kartverket.MetadataEditor.Startup))]
+namespace Kartverket.MetadataEditor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
