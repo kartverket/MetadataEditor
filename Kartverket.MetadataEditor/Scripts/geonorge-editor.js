4409ccb67b0bf9a980b125d6661179d8f266c2b6
Henning Jensen <henning@arkitektum.no>
2014-05-12 11:25:11 +0200

    Disable enter key to avoid unintentional form submits and weird behaviour in keyword form elements.

---
+++ Kartverket.MetadataEditor/Scripts/geonorge-editor.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    /* disable enter key to avoid accidental form submit */
+    $(document).keypress(function (event) {
+        if (event.which == '13') { // 13 = enter key
+            event.preventDefault();
+        }
+    });
+});
