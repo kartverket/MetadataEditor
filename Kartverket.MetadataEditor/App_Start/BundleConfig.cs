6d404217c0fcb0dd981f664e3f77bca491d50f92
Henning Jensen <henning@arkitektum.no>
2014-05-14 12:31:46 +0200

    Fix problem with bundling of jquery-ui css.

--- Kartverket.MetadataEditor/App_Start/BundleConfig.cs
+++ Kartverket.MetadataEditor/App_Start/BundleConfig.cs
@@ -5,26 +5,8 @@ namespace Kartverket.MetadataEditor
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-/*    
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-            */
-
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.validate*",
@@ -38,20 +20,15 @@ namespace Kartverket.MetadataEditor
                 "~/Scripts/jQuery.FileUpload/jquery.fileupload.js",
                 "~/Scripts/geonorge-editor.js"));
 
-
-            bundles.Add(new StyleBundle("~/Content/themes/base").Include(
-                    "~/Content/themes/base/jquery.ui.core.css",
-                    "~/Content/themes/base/jquery.ui.datepicker.css",
-                    "~/Content/themes/base/jquery.ui.theme.css"
-                ));
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                "~/Content/themes/base/jquery.ui.core.css",
+                "~/Content/themes/base/jquery.ui.datepicker.css",
+                "~/Content/themes/base/jquery.ui.theme.css"));
                 
-
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-            
-                      "~/Content/bootstrap.css",
-                      "~/Content/geonorge-default.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/geonorge-default.css",
+                "~/Content/site.css"));
         }
     }
 }
