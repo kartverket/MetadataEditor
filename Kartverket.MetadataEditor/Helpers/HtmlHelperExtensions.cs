7bf24ef89022d54a892a39ea8bc2b7bb0c19eb38
Henning Jensen <henning@arkitektum.no>
2014-11-18 15:21:00 +0100

    Add build version number.

---
+++ Kartverket.MetadataEditor/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Kartverket.MetadataEditor.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString ApplicationVersionNumber(this HtmlHelper helper)
+        {
+            string versionNumber = WebConfigurationManager.AppSettings["BuildVersionNumber"];
+            return MvcHtmlString.Create(versionNumber);
+        }
+    }
+}
