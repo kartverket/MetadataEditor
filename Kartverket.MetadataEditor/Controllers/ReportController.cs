8b30a9c26b1eece7ecbe704ca44004c0f267011c
Dag Olav <dagolav@arkitektum.no>
2015-09-09 13:45:42 +0200

    Report service

---
+++ Kartverket.MetadataEditor/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using Kartverket.MetadataEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kartverket.MetadataEditor.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            List<MetadataViewModel> metadata = new ReportService().Report1();
+            return View(metadata);
+        }
+    }
+}
---
+++ Kartverket.MetadataEditor/Models/ReportService.cs
@@ -0,0 +1,70 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.MetadataEditor.Models
+{
+    public class ReportService
+    {
+        private MetadataService _metadataService;
+        private static readonly ILog Log = LogManager.GetLogger(typeof(MvcApplication));
+
+        public ReportService() 
+        {
+            _metadataService = new MetadataService();
+        }
+
+        public List<MetadataViewModel> Report1()
+        {
+
+            List<MetadataViewModel> report = new List<MetadataViewModel>();
+
+            MetadataIndexViewModel model = new MetadataIndexViewModel();
+            int offset = 1;
+            int limit = 50;
+            model = _metadataService.SearchMetadata("", "", offset, limit);
+
+            foreach (var item in model.MetadataItems)
+            {
+                //if (item.Uuid != "b7dd7cbb-5cd2-4124-bf9c-fc83f9579982") 
+                //{ 
+                MetadataViewModel md = _metadataService.GetMetadataModel(item.Uuid);
+                if(md.IsDataset())
+                report.Add(md);
+                //}
+            }
+
+            int numberOfRecordsMatched = model.TotalNumberOfRecords;
+            int next = model.OffsetNext();
+
+            
+                while (next < numberOfRecordsMatched)
+                {
+                    
+                    model = _metadataService.SearchMetadata("", "", next, limit);
+
+                    foreach (var item in model.MetadataItems)
+                    {
+                        try
+                        {
+                        MetadataViewModel md = _metadataService.GetMetadataModel(item.Uuid);
+                        if (md.IsDataset())
+                            report.Add(md);
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Error(e.Message);
+                        }
+                    }
+
+                    next = model.OffsetNext();
+                    if (next == 0) break;
+                }
+
+            return report;
+        }
+
+    }
+}
