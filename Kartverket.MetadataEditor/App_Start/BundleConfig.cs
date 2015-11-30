811a0c40c9811cb070292c3e6cbdd110199bc6da
Dag Olav <dagolav@arkitektum.no>
2015-11-30 10:15:10 +0100

    Initialize new layout

--- Kartverket.MetadataEditor/App_Start/BundleConfig.cs
+++ Kartverket.MetadataEditor/App_Start/BundleConfig.cs
@@ -8,22 +8,22 @@ namespace Kartverket.MetadataEditor
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                "~/Scripts/jquery-{version}.js",
+                //"~/Scripts/jquery-{version}.js",*/
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.validate.unobtrusive.js",
                 "~/Scripts/expressive.annotations.validate.js",
                 "~/Scripts/globalize.js",
                 "~/Scripts/globalize.culture.nb-NO.js",
                 "~/Scripts/globalize-custom.js",
-                "~/Scripts/bootstrap.js",
+                //"~/Scripts/bootstrap.js",*/
                 "~/Scripts/respond.js",
                 "~/Scripts/jquery-ui-{version}.js",
                 "~/Scripts/jQuery.FileUpload/jquery.iframe-transport.js",
                 "~/Scripts/jQuery.FileUpload/jquery.fileupload.js",
                 "~/Scripts/bootstrap-filestyle.js",
                 "~/Scripts/geonorge-editor.js",
-                "~/Scripts/jquery.autosize.js",
-                "~/Scripts/jquery.cookie.js"));
+                "~/Scripts/jquery.autosize.js"/*,
+                "~/Scripts/jquery.cookie.js"*/));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                 "~/Content/themes/base/core.css",
@@ -31,10 +31,10 @@ namespace Kartverket.MetadataEditor
                 "~/Content/themes/base/theme.css"));
               
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/common.css",
-                "~/Content/navbar.css",
-                "~/Content/searchbar.css",
+                //"~/Content/bootstrap.css",
+                //"~/Content/common.css",
+                //"~/Content/navbar.css",
+                //"~/Content/searchbar.css",
                 "~/Content/site.css"));
         }
     }
