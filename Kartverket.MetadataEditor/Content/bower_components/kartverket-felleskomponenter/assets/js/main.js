e3dda63425a55705aa373f482b568313da8c3dcb
Benjamin Dehli <benjamin@arkitektum.no>
2016-04-22 11:36:04 +0200

    Update Bower components

--- Kartverket.MetadataEditor/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
+++ Kartverket.MetadataEditor/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
@@ -694,3 +694,22 @@ function updateShoppingCartCookie() {
 $(window).load(function () {
     updateShoppingCart();
 });
+function setActiveTab(tabLink){
+	var tab = tabLink.data('tab');
+	localStorage.setItem("metadata-tab", tab);
+	$(".localstorage-tabs li.active").removeClass('active');
+	tabLink.parent('li').addClass('active');
+}
+function getActiveTab(){
+	var tab = localStorage.getItem("metadata-tab");
+	var tabLink = $(".localstorage-tabs li a[data-tab='" + tab + "']");
+	setActiveTab(tabLink);
+}
+$(".localstorage-tabs").ready(function () {
+	getActiveTab();
+	$(".localstorage-tabs li a").click(function (event){
+		event.preventDefault();
+		setActiveTab($(this));
+		window.location.href = $(this).prop('href');
+	});
+});
