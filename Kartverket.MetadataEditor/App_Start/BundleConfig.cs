0145150540b56060687950bf5f058fd76b1b8136
Benjamin Dehli <benjamin@arkitektum.no>
2016-06-15 09:46:40 +0200

    Add fonts from bower component

--- Kartverket.MetadataEditor/App_Start/BundleConfig.cs
+++ Kartverket.MetadataEditor/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace Kartverket.MetadataEditor
         {
             bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
                ));
 
