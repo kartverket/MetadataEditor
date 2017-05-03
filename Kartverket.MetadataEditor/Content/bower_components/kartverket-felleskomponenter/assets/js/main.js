cbe0d2289a5ea0398e3235f4422e5ea1a5b7261c
Benjamin Dehli <benjamin@arkitektum.no>
2017-05-03 09:51:39 +0200

    Update Bower components

--- Kartverket.MetadataEditor/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
+++ Kartverket.MetadataEditor/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
@@ -921,3 +921,26 @@ function disableLastBreadcrumb(){
 $("document").ready( function(){
 	disableLastBreadcrumb();
 });
+$(document).on('focus', '.custom-select-list-input', function() {
+    var customSelectListElement = $(this).closest('.custom-select-list');
+    var dropdownElement = customSelectListElement.find('.custom-select-list-dropdown');
+    dropdownElement.addClass('active');
+    dropdownElement.removeClass('transparent');
+});
+
+$(document).on('blur', '.custom-select-list-input', function() {
+    var inputElement = this;
+    var customSelectListElement = $(this).closest('.custom-select-list');
+    var dropdownElement = customSelectListElement.find('.custom-select-list-dropdown');
+    dropdownElement.addClass("transparent");
+    setTimeout(function() {
+        if (inputElement !== document.activeElement) {
+            dropdownElement.removeClass("active");
+            dropdownElement.removeClass("transparent");
+        }
+    }, 1000);
+});
+
+$(document).on('click', '.custom-select-list-input-container', function() {
+    $(this).find('.custom-select-list-input').focus();
+});
