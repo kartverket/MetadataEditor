e53dbea650a924ac25aaa1e8ebda444e451b5e8f
Benjamin Dehli <benjamin@arkitektum.no>
2018-02-12 09:25:31 +0100

    Implement node module for Geonorge.SharedPartials

---
+++ Kartverket.MetadataEditor/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require("gulp"),
+    bundleTask = require('./node_modules/geonorge-shared-partials/gulp-tasks/bundle')(gulp),
+    config = {
+        url: './node_modules/geonorge-shared-partials/bundle.config.js',
+        dist: 'dist'
+    };
+
+gulp.task("default", function () { return bundleTask(config) });
+gulp.task("test", function () { bundleTask(config, "test") });
+gulp.task("prod", function () { bundleTask(config, "prod") });
