5bb3f2192f550a9e7b3abb55f6918644a6981827
Dag Olav Dahle <dagolav@arkitektum.no>
2018-09-11 16:08:02 +0200

    Initial conform technical specification

---
+++ Kartverket.MetadataEditor/Models/InspireCodelist/TechnicalSpecification.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.MetadataEditor.Models.InspireCodelist
+{
+    public class TechnicalSpecification
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string PublicationDate { get; set; }
+    }
+
+    public class Technical
+    {
+        private static List<TechnicalSpecification> _technicalSpesifications = new List<TechnicalSpecification>();
+
+        public static List<TechnicalSpecification> GetSpecifications
+        {
+            get
+            {
+                return _technicalSpesifications;
+            }
+            set {  }
+        }
+
+        public static TechnicalSpecification GetSpecification(string name)
+        {
+            return _technicalSpesifications.Where(s => s.Name == name).FirstOrDefault();
+        }
+
+        public Technical()
+        {
+            AddSpesifications();
+        }
+
+        private void AddSpesifications()
+        {
+            _technicalSpesifications.Add(
+                new TechnicalSpecification
+                {
+                    Name = "EN ISO 19128:2005(E): Geographic information — Web map server interface",
+                    Url = "http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=32546",
+                    PublicationDate = "2005-12-01"
+                }); 
+        }
+    }
+}
--- Kartverket.MetadataEditor/Models/MetadataService.cs
+++ Kartverket.MetadataEditor/Models/MetadataService.cs
@@ -1286,8 +1286,57 @@ namespace Kartverket.MetadataEditor.Models
                         });
                     }
 
+                    //start conform technical specification
 
-                    if(!string.IsNullOrEmpty(model.QualitySpecificationTitleInspireSpatialServiceConformance))
+                    var technicalSpesification = Technical.GetSpecification(model.QualitySpecificationTitleInspireSpatialServiceTechnicalConformance);
+
+                    if (model.QualitySpecificationResultInspireSpatialServiceTechnicalConformance == true)
+                    {
+                        qualityList.Add(new SimpleQualitySpecification
+                        {
+                            Title = technicalSpesification.Name,
+                            TitleLink = technicalSpesification.Url,
+                            Date = technicalSpesification.PublicationDate,
+                            DateType = dateType,
+                            Explanation = "Denne romlige datatjenesten er i overensstemmelse med " + technicalSpesification.Name + " spesifikasjonen",
+                            EnglishExplanation = "This Spatial Data Service set is conformant with the " + technicalSpesification.Name +  " specification",
+                            Result = true,
+                            Responsible = "conformity-to-technical-specification"
+                        });
+                    }
+                    else if (model.QualitySpecificationResultInspireSpatialServiceTechnicalConformance == false)
+                    {
+                        qualityList.Add(new SimpleQualitySpecification
+                        {
+                            Title = technicalSpesification.Name,
+                            TitleLink = technicalSpesification.Url,
+                            Date = technicalSpesification.PublicationDate,
+                            DateType = dateType,
+                            Explanation = "Denne romlige datatjenesten er ikke i overensstemmelse med " + technicalSpesification.Name + " spesifikasjonen",
+                            EnglishExplanation = "This Spatial Data Service set is not conformant with the " + technicalSpesification.Name + " specification",
+                            Result = false,
+                            Responsible = "conformity-to-technical-specification"
+                        });
+
+                    }
+                    else
+                    {
+                        qualityList.Add(new SimpleQualitySpecification
+                        {
+                            Title = technicalSpesification.Name,
+                            TitleLink = technicalSpesification.Url,
+                            Date = technicalSpesification.PublicationDate,
+                            DateType = dateType,
+                            Explanation = "Denne romlige datatjenesten er ikke evaluert i overensstemmelse med " + technicalSpesification.Name + " spesifikasjonen",
+                            EnglishExplanation = "This Spatial Data Service set is not evaluated conformant with the " + technicalSpesification.Name + " specification",
+                            Result = null,
+                            Responsible = "conformity-to-technical-specification"
+                        });
+                    }
+                    //end conform technical spesification
+
+
+                    if (!string.IsNullOrEmpty(model.QualitySpecificationTitleInspireSpatialServiceConformance))
                     {
                         string sds = model.QualitySpecificationTitleInspireSpatialServiceConformance.ToLower();
                         string Sds = CapitalizeFirstLetter(sds);
