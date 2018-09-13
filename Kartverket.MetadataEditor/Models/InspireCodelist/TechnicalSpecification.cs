296557f1184880885520586a7876193b56fc2a4c
Dag Olav Dahle <dagolav@arkitektum.no>
2018-09-13 15:53:22 +0200

    Get codelist for technical specifications from registry

--- Kartverket.MetadataEditor/Models/InspireCodelist/TechnicalSpecification.cs
+++ Kartverket.MetadataEditor/Models/InspireCodelist/TechnicalSpecification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,15 +15,33 @@ namespace Kartverket.MetadataEditor.Models.InspireCodelist
 
     public class Technical
     {
-        private static List<TechnicalSpecification> _technicalSpesifications = new List<TechnicalSpecification>()
+        private static List<TechnicalSpecification> _technicalSpesifications = GetCodeList();
+
+        private static List<TechnicalSpecification> GetCodeList()
         {
-             new TechnicalSpecification
-                {
-                    Name = "EN ISO 19128:2005(E): Geographic information — Web map server interface",
-                    Url = "http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=32546",
-                    PublicationDate = "2005-12-01"
-                }
-        };
+            List<TechnicalSpecification> technicalSpecifications = new List<TechnicalSpecification>();
+
+            string url = System.Web.Configuration.WebConfigurationManager.AppSettings["RegistryUrl"] + "api/subregister/metadata-kodelister/kartverket/tjenestestandarder";
+            System.Net.WebClient c = new System.Net.WebClient();
+            c.Encoding = System.Text.Encoding.UTF8;
+            var data = c.DownloadString(url);
+            var response = Newtonsoft.Json.Linq.JObject.Parse(data);
+
+            var codeList = response["containeditems"];
+
+            foreach (var code in codeList)
+            {
+                JToken codevalueToken = code["codevalue"];
+                string codeValue = codevalueToken?.ToString();
+                string name = code["label"].ToString();
+                DateTime validFromDate = code["ValidFrom"].ToObject<DateTime>();
+                string validFrom = validFromDate.ToString("yyyy-MM-dd");
+
+                technicalSpecifications.Add(new TechnicalSpecification { Name = name, Url = codeValue, PublicationDate = validFrom });
+            }
+
+            return technicalSpecifications;
+        }
 
         public static List<TechnicalSpecification> GetSpecifications
         {
