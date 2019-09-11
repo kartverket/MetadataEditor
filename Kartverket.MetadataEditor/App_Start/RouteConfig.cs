d7cf175e3c434c8120d61cd35db9e4cf46965e9c
Dag Olav Dahle <dagolav@arkitektum.no>
2019-09-11 10:55:51 +0200

    Adjust login

--- Kartverket.MetadataEditor/App_Start/RouteConfig.cs
+++ Kartverket.MetadataEditor/App_Start/RouteConfig.cs
@@ -13,12 +13,17 @@ namespace Kartverket.MetadataEditor
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("SignIn", "SignIn", new { controller = "Home", action = "SignIn" });
+            routes.MapRoute("SignOut", "SignOut", new { controller = "Home", action = "SignOut" });
+            // authentication - openid connect 
+            routes.MapRoute("OIDC-callback-signout", "signout-callback-oidc", new { controller = "Home", action = "SignOutCallback" });
+
             routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Metadata", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
