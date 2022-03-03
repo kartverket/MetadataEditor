ab587f3c0aab25c941483221feae01d6fc594900
Dag Olav Dahle <dagolav@arkitektum.no>
2022-03-03 16:46:40 +0100

    Add label and help text for spatial scope

--- Kartverket.MetadataEditor/Resources/UI.designer.cs
+++ Kartverket.MetadataEditor/Resources/UI.designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UI {
@@ -2615,6 +2615,24 @@ namespace Resources {
         public static string SpatialRepresentationType {
             get {
                 return ResourceManager.GetString("SpatialRepresentationType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Geografisk nivå.
+        /// </summary>
+        public static string SpatialScope {
+            get {
+                return ResourceManager.GetString("SpatialScope", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Administrativt nivå som datasettet har til hensikt å dekke.
+        /// </summary>
+        public static string SpatialScopeHelp {
+            get {
+                return ResourceManager.GetString("SpatialScopeHelp", resourceCulture);
             }
         }
         
