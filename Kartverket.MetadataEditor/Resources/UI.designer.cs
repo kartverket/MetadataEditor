499c32f8179fc89c957a946fda3d55ba89812fd1
Dag Olav Dahle <dagolav@arkitektum.no>
2025-12-29 18:26:17 +0100

    Do not allow save if error is bad data like illegal dataset id/duplicate

--- Kartverket.MetadataEditor/Resources/UI.designer.cs
+++ Kartverket.MetadataEditor/Resources/UI.designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UI {
@@ -589,6 +589,15 @@ namespace Resources {
         public static string ErrorDuplicateDatasetName {
             get {
                 return ResourceManager.GetString("ErrorDuplicateDatasetName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ulovlig verdi.
+        /// </summary>
+        public static string ErrorIllegalCharacter {
+            get {
+                return ResourceManager.GetString("ErrorIllegalCharacter", resourceCulture);
             }
         }
         
